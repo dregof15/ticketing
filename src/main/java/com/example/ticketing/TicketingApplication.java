4b936141a8b15391b80428ff52d8918b8a85edbe
Parksunghyun <49517121+dregof15@users.noreply.github.com>
2023-11-15 16:13:49 +0900

    비행기 예매 프로그램 개발 시작

---
+++ src/main/java/com/example/ticketing/TicketingApplication.java
@@ -0,0 +1,15 @@
+package com.example.ticketing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketingApplication.class, args);
+
+        System.out.println("깃 확인중");
+    }
+
+}
