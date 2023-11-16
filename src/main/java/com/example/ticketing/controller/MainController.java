4738283789b1846c9dec67c4122e8f1e2434dfec
Parksunghyun <49517121+dregof15@users.noreply.github.com>
2023-11-16 14:16:45 +0900

    비행기 예매 프로그램 개발 css 및 기본 html 수정

---
+++ src/main/java/com/example/ticketing/controller/MainController.java
@@ -0,0 +1,18 @@
+package com.example.ticketing.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")  // get post 등 여러 매핑을 포함해 다 이동 용도
+@RequiredArgsConstructor // final 생성자 자동 생성
+public class MainController {
+    @GetMapping
+    public String mainpage () {
+
+        return "main/main";
+    }
+
+}
---
+++ src/main/java/com/example/ticketing/controller/MemberController.java
@@ -0,0 +1,13 @@
+package com.example.ticketing.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/member")
+public class MemberController {
+
+    //@GetMapping
+
+}
